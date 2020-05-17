f60af67db1a47f5f2e88e45ee95a9fd7767f2c34
Yifu (Jason) He <37979856+heyifuwhu@users.noreply.github.com>
2020-05-17 18:27:32 -0400

    Add files via upload

---
+++ C# Code - Copy/Chapter 1 Introduction/Heston_Price_Gauss_Laguerre/MainProgram.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.IO;
+
+namespace Heston_Price_Gauss_Laguerre
+{
+    class HestonPriceGaussLaguerre
+    {
+        static void Main(string[] args)
+        {
+            // 32-point Gauss-Laguerre Abscissas and weights
+            double[] x = new Double[32];
+            double[] w = new Double[32];
+            using(TextReader reader = File.OpenText("../../GaussLaguerre32.txt"))
+            {
+                for(int k=0;k<=31;k++)
+                {
+                    string text = reader.ReadLine();
+                    string[] bits = text.Split(' ');
+                    x[k] = double.Parse(bits[0]);
+                    w[k] = double.Parse(bits[1]);
+                }
+            }
+            // Heston parameters
+            HParam param = new HParam();
+            param.kappa = 1.5;
+            param.theta = 0.04;
+            param.sigma = 0.3;
+            param.v0 = 0.05412;
+            param.rho = -0.9;
+            param.lambda = 0.0;
+
+            // Option settings
+            OpSet settings = new OpSet();
+            settings.S = 101.52;
+            settings.K = 100.0;
+            settings.T = 0.15;
+            settings.r = 0.02;
+            settings.q = 0.0;
+            settings.PutCall = "C";
+            settings.trap = 1;
+
+            // The Heston price
+            HestonPrice HP = new HestonPrice();
+            double Price = HP.HestonPriceGaussLaguerre(param,settings,x,w);
+            Console.WriteLine("Heston price using 32-point Gauss Laguerre");
+            Console.WriteLine("------------------------------------------ ");
+            Console.WriteLine("Option Flavor =  {0,0:F5}",settings.PutCall);
+            Console.WriteLine("Strike Price  =  {0,0:0}" ,settings.K);
+            Console.WriteLine("Maturity      =  {0,0:F2}",settings.T);
+            Console.WriteLine("Price         =  {0,0:F4}",Price);
+            Console.WriteLine("------------------------------------------ ");
+            Console.WriteLine(" ");
+        }
+    }
+}
---
+++ C# Code - Copy/Chapter 1 Introduction/Heston_Price_Gauss_Laguerre_Consolidated/Structures.cs
@@ -0,0 +1,23 @@
+﻿// Heston parameters
+public struct HParam
+{
+    public double kappa;        // Mean reversion speed
+    public double theta;        // Mean reversion level
+    public double sigma;        // Volatility of variance
+    public double v0;           // Initial variance
+    public double rho;          // Correlation
+    public double lambda;       // Risk parameter
+}
+
+// Settings for the option price calculation
+public struct OpSet
+{
+    public double S;            // Spot price
+    public double K;            // Strke price
+    public double T;            // Maturity
+    public double r;            // Risk free rate
+    public double q;            // Dividend
+    public string PutCall;      // "P"ut or "C"all
+    public int trap;            // 1="Little Trap" characteristic function; 2=Original Heston c.f.
+}
+

1f4e73dbbf920c3b3d0f7e595f63b1238f68610f
Yifu (Jason) He <37979856+heyifuwhu@users.noreply.github.com>
2020-05-17 18:48:28 -0400

    Add files via upload

---
+++ file/C sharp Code - Copy/Chapter 5 Numerical Integration/Heston_FRFT/MainProgram.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.IO;
+
+
+namespace Heston_FRFT
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            // 32-point Gauss-Laguerre Abscissas and weights
+            double[] x = new Double[32];
+            double[] w = new Double[32];
+            using(TextReader reader = File.OpenText("../../GaussLaguerre32.txt"))
+            {
+                for(int k=0;k<=31;k++)
+                {
+                    string text = reader.ReadLine();
+                    string[] bits = text.Split(' ');
+                    x[k] = double.Parse(bits[0]);
+                    w[k] = double.Parse(bits[1]);
+                }
+            }
+            // Heston parameters
+            HParam param = new HParam();
+            param.kappa = 0.2;
+            param.theta = 0.05;
+            param.sigma = 0.3;
+            param.v0 = 0.05;
+            param.rho = -0.7;
+            param.lambda = 0.0;
+
+            // Option settings
+            OpSet settings = new OpSet();
+            settings.S = 50.0;
+            settings.T = 0.5;
+            settings.r = 0.03;
+            settings.q = 0.05;
+            settings.PutCall = "C";
+            settings.trap = 1;
+
+            // FFT settings
+            double M = Math.Pow(2,10);
+            int N = Convert.ToInt16(M);
+            double alpha = 1.5;
+            double uplimit = 100.0;
+            string rule;
+            
+            // Set the integration increment
+            double eta = 1e-2;
+
+            // Set the log-strike increment
+            double lambdainc = 1e-2;
+
+            // Alternative: Set the log-strike increment so that the strike range is Spot +/ 5
+            //double lambdainc = 2.0/M*Math.Log(settings.S/(settings.S - 5.0));
+
+            // Obtain the FFT strikes and calls using the Trapezoidal rule
+            HFRFT FRFT = new HFRFT();
+            double[,] PriceTrapz = new double[N,2];
+            rule = "Trapezoidal";
+            PriceTrapz = FRFT.HestonFRFT(N,uplimit,alpha,rule,lambdainc,eta,param,settings);
+
+            // Obtain the FFT strikes and calls using Simpson's rule
+            double[,] PriceSimps = new double[N,2];
+            rule = "Simpsons";
+            PriceSimps = FRFT.HestonFRFT(N,uplimit,alpha,rule,lambdainc,eta,param,settings);
+
+            // Obtain the extact price and compare to FFT price
+            HestonPrice HP = new HestonPrice();
+            double[] PriceHeston = new double[N];
+            double[] ErrorTrapz = new double[N];
+            double[] ErrorSimps = new double[N];
+            for(int j=0;j<=N-1;j++)
+            {
+                settings.K = PriceTrapz[j,0];
+                PriceHeston[j] = HP.HestonPriceGaussLaguerre(param,settings,x,w);
+                ErrorTrapz[j] = (PriceTrapz[j,1] - PriceHeston[j])/PriceHeston[j]*100.0;
+                ErrorSimps[j] = (PriceSimps[j,1] - PriceHeston[j])/PriceHeston[j]*100.0;
+            }
+
+            // Output the results near the money
+            int start = N/2 - 3;
+            int end   = N/2 + 3;
+            Console.WriteLine("FRFT strike range from {0:F2} to {1:F2}",PriceTrapz[0,0],PriceTrapz[N-1,0]);
+            Console.WriteLine();
+            Console.WriteLine("Sample Strikes");
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine("Strike        Exact     FRFT Trapz  FRFT Simpson  %ErrorTrapz  %ErrorSimpson");
+            Console.WriteLine("---------------------------------------------------------------------------");
+            for(int j=start;j<=end;j++)
+                Console.WriteLine("{0:F4} {1,12:F4} {2,12:F4} {3,12:F4} {4,12:F4} {5,12:F4}",PriceTrapz[j,0],PriceHeston[j],PriceTrapz[j,1],PriceSimps[j,1],ErrorTrapz[j],ErrorSimps[j]);
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine("Integration increment {0:F10}",eta);
+            Console.WriteLine("Log strike increment {0:F10}",lambdainc);
+        }
+    }
+}
+
+
---
+++ file/C sharp Code - Copy/Chapter 5 Numerical Integration/Heston_Quadratures/MainProgram.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.IO;
+
+namespace Heston_Quadratures
+{
+    class NewtonCotesIntegration
+    {
+        static void Main(string[] args)
+        {
+            // 32-point Gauss-Laguerre Abscissas and weights
+            double[] xGLa = new Double[32];
+            double[] wGLa = new Double[32];
+            using(TextReader reader = File.OpenText("../../GaussLaguerre32.txt"))
+            {
+                for(int k=0;k<=31;k++)
+                {
+                    string text = reader.ReadLine();
+                    string[] bits = text.Split(' ');
+                    xGLa[k] = double.Parse(bits[0]);
+                    wGLa[k] = double.Parse(bits[1]);
+                }
+            }
+            // 32-point Gauss-Legendre Abscissas and weights
+            double[] xGLe = new Double[32];
+            double[] wGLe = new Double[32];
+            using(TextReader reader = File.OpenText("../../GaussLegendre32.txt"))
+            {
+                for(int k=0;k<=31;k++)
+                {
+                    string text = reader.ReadLine();
+                    string[] bits = text.Split(' ');
+                    xGLe[k] = double.Parse(bits[0]);
+                    wGLe[k] = double.Parse(bits[1]);
+                }
+            }
+            // 32-point Gauss-Lobatto Abscissas and weights
+            double[] xGLo = new Double[32];
+            double[] wGLo = new Double[32];
+            using(TextReader reader = File.OpenText("../../GaussLobatto32.txt"))
+            {
+                for(int k=0;k<=31;k++)
+                {
+                    string text = reader.ReadLine();
+                    string[] bits = text.Split(' ');
+                    xGLo[k] = double.Parse(bits[0]);
+                    wGLo[k] = double.Parse(bits[1]);
+                }
+            }
+
+            // Heston parameters
+            HParam param = new HParam();
+            param.kappa = 2.0;
+            param.theta = 0.05;
+            param.sigma = 0.3;
+            param.v0 = 0.035;
+            param.rho = -0.9;
+            param.lambda = 0.0;
+
+            // Option price settings
+            OpSet settings = new OpSet();
+            settings.S = 100.0;
+            settings.T = 0.25;
+            settings.r = 0.05;
+            settings.q = 0.02;
+            settings.trap = 1;
+
+            // Lower and upper integration limits
+            double a = 0.0;         // Lower Limit for Gauss Legendre and Newton Coates
+            double b = 100.0;       // Upper Limit for Gauss Legendre
+            double A = 1e-5;        // Lower Limit for Gauss Lobatto
+            double B = 100.0;        // Upper Limit for Gauss Lobatto
+
+            int N = 5000;           // Number of points for Newton Coates quadratures
+
+            // Range of strikes and option flavor
+            double[] Strikes = new double[] { 90.0,95.0,100.0,105.0,110.0 };
+            settings.PutCall = "C";
+
+            // Initialize the price vectors
+            int M = Strikes.Length;
+            double[] PriceGLa     = new double[M];
+            double[] PriceGLe     = new double[M];
+            double[] PriceGLo     = new double[M];
+            double[] PriceMP      = new double[M];
+            double[] PriceTrapz   = new double[M];
+            double[] PriceSimp    = new double[M];
+            double[] PriceSimp38  = new double[M];
+
+            // Obtain the prices and output to console
+            HestonPrice HP = new HestonPrice();
+            NewtonCotes NC = new NewtonCotes();
+            Console.WriteLine("Using {0:0} points for Newton Cotes and 32 points for quadrature ",N);
+            Console.WriteLine(" ");
+            for(int k=0;k<=M-1;k++)
+            {
+                settings.K = Strikes[k];
+                PriceGLa[k] = HP.HestonPriceGaussLaguerre(param,settings,xGLa,wGLa);           // Gauss Laguerre
+                PriceGLe[k] = HP.HestonPriceGaussLegendre(param,settings,xGLe,wGLe,a,b);       // Gauss Legendre
+                PriceGLo[k] = HP.HestonPriceGaussLegendre(param,settings,xGLo,wGLo,A,B);       // Gauss Lobatto
+                PriceMP[k]     = NC.HestonPriceNewtonCotes(param,settings,1,a,b,N);           // Mid point rule
+                PriceTrapz[k]  = NC.HestonPriceNewtonCotes(param,settings,2,A,B,N);           // Trapezoidal rule
+                PriceSimp[k]   = NC.HestonPriceNewtonCotes(param,settings,3,A,B,N);           // Simpson's Rule
+                PriceSimp38[k] = NC.HestonPriceNewtonCotes(param,settings,4,A,B,N);           // Simpson's 3/8 rule
+                Console.Write("Strike {0,2}", Strikes[k]);
+                Console.WriteLine(" ------------------");
+                Console.WriteLine("Mid Point       {0:F5}",PriceMP[k]);
+                Console.WriteLine("Trapezoidal     {0:F5}",PriceTrapz[k]);
+                Console.WriteLine("Simpson's       {0:F5}",PriceSimp[k]);
+                Console.WriteLine("Simpson's 3/8   {0:F5}",PriceSimp38[k]);
+                Console.WriteLine("Gauss Laguerre  {0:F5}",PriceGLa[k]);
+                Console.WriteLine("Gauss Legendre  {0:F5}",PriceGLe[k]);
+                Console.WriteLine("Gauss Lobatto   {0:F5}",PriceGLo[k]);
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
+
+
+/*
+namespace Heston_Quadratures
+{
+    public partial class Quadratures
+    {
+        static void Main(string[] args)
+        {
+
+            // Heston parameters
+            HParam param = new HParam();
+            param.kappa = 5.0;
+            param.theta = 0.16;
+            param.sigma = 0.9;
+            param.v0 = 0.0625;
+            param.rho = 0.1;
+            param.lambda = 0.0;
+
+            // Clarke and Parrott option price settings, but for European puts
+            OpSet settings = new OpSet();
+            settings.K = 10.0;
+            settings.T = 0.25;
+            settings.r = 0.1;
+            settings.q = 0.0;
+            settings.trap = 1;
+
+            // Lower and upper integration limits
+            double A = 1e-10;        // Lower Limit
+            double B = 200.0;        // Upper Limit 
+            int N = 50000;           // Number of points 
+
+            // Range of strikes and option flavor
+            double[] Spot= new double[] { 8.0,9.0,10.0,11.0,12.0 };
+            settings.PutCall = "P";
+
+            // Initialize the price vectors
+            int M = Spot.Length;
+            double[] PriceTrapz  = new double[M];
+            double[] PriceSimp   = new double[M];
+            double[] PriceSimp38 = new double[M];
+
+            // Obtain the prices and output to console
+            for(int k=0;k<=M-1;k++)
+            {
+                settings.S = Spot[k];
+                PriceTrapz[k]  = HestonPriceNewtonCoates(param,settings,2,A,B,N);           // Trapezoidal rule
+                PriceSimp[k]   = HestonPriceNewtonCoates(param,settings,3,A,B,N);           // Simpson's Rule
+                PriceSimp38[k] = HestonPriceNewtonCoates(param,settings,4,A,B,N);           // Simpson's 3/8 rule
+                Console.Write("Strike {0,2}",Spot[k]);
+                Console.WriteLine(" ------------------");
+                Console.WriteLine("Trapezoidal     {0:F6}",PriceTrapz[k]);
+                Console.WriteLine("Simpson's       {0:F6}",PriceSimp[k]);
+                Console.WriteLine("Simpson's 3/8   {0:F6}",PriceSimp38[k]);
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
+
+*/
+
